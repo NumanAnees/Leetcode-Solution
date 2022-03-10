79861ea7e3d142cddce518c8777637f3672261f5
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-03-11 01:51:38 +0500

    Rotate an array

---
+++ Learning/Arrays/Solutions Code/Rotate an Array/N square approach O(1) space.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+       
+    for(int i=0;i<k;i++)
+    {
+        int end = nums.size()-1;
+        int endP = nums.size()-2;
+        while(endP>=0)
+        {
+            swap(nums[end],nums[endP]);
+            end--;
+            endP--;
+        }
+      }
+    }
+};
