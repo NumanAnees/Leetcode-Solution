bca6b3c7cb63e993fda1e18ed9cc3ba9fe44607e
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-03-19 13:21:59 +0500

    String Compression

---
+++ Learning/Strings/Solutions Code/String Compression/index.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+     int i=0;
+     int ansIndex=0;
+     int n = chars.size();
+    while(i<n)
+    {
+       int j=i+1;
+        while(j<n && chars[j] ==chars[i])
+        {
+            j++;
+        }
+        //store in old chars vector
+        chars[ansIndex++] = chars[i];
+        int count = j-i;
+        if(count>1)
+        {
+            string cnt = to_string(count);
+            for(char ch: cnt)
+            {
+                chars[ansIndex++]=ch;
+            }
+        }
+        i=j;
+    }
+     //return the index size not the vector size
+     return ansIndex;
+    }
+};
