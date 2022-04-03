fd373d1ce2fac160d90a493b63b4bf39d24d93e6
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-04-03 19:37:50 +0500

    Buy and Sell stocks

---
+++ Codes/Arrays/Solutions Code/Best Time to Buy and Sell Stock/README.md
@@ -0,0 +1,8 @@
+# Problem
+
+**https://leetcode.com/problems/best-time-to-buy-and-sell-stock/**
+
+# Solution
+
+By selling on each day decide which day gives `Maximum Profit` if buying day is minimum of 0 and current like.
+`min(prices[0],current)`
---
+++ Codes/Arrays/Solutions Code/Best Time to Buy and Sell Stock/index.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+    int maxProfit = 0;
+    int minSofar = prices[0];
+    for(int i=0;i<prices.size();i++)
+    {
+        minSofar = min(minSofar,prices[i]);
+        int profit = prices[i]-minSofar;
+        maxProfit=max(maxProfit,profit);
+    }
+        return maxProfit;
+    }
+};
+//-------------------------------------Aproach-2-----------------------------------
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minProfit = INT_MAX;
+        int maxProfit = 0;
+        for (auto &val : prices) {
+            minProfit = min(val, minProfit);
+            maxProfit = max((val - minProfit), maxProfit);
+        }
+        return maxProfit;
+    }
+};
