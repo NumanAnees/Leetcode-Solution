24ee3635fee08dc6b85fd96cc2138efdbcd2492a
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-04-24 06:00:52 +0500

    Fibonacci Number

---
+++ Codes/Recursion/Fibonacci Number/index.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+       //base case 
+        if(n == 0){
+            return 0;
+        }
+        else if(n==1)
+        {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+};
