a45af721cfa87e2021bdadb24991b6bb2917dfe6
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-10-22 01:54:54 +0500

    Remove outermost parantheses

---
+++ Arrays/Solutions Code/Remove Outermost Parantheses/Readme.md
@@ -0,0 +1,9 @@
+## Problem
+
+**https://leetcode.com/problems/remove-outermost-parentheses/**
+
+## Solution
+
+- https://leetcode.com/problems/remove-outermost-parentheses/discuss/270022/JavaC%2B%2BPython-Count-Opened-Parenthesis
+
+## Dry Run
---
+++ Arrays/Solutions Code/Remove Outermost Parantheses/index.cpp
@@ -0,0 +1,38 @@
+//---------------------------------------Better Approach-----------------------------------------------------
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        string res;
+        int opened = 0;
+        for (char c : S) {
+            if (c == '(' && opened++ > 0) res += c;
+            if (c == ')' && opened-- > 1) res += c;
+        }
+        return res;
+    }
+};
+
+//----------------------------My approach work perfectly for 1 child-----------------------------------------
+
+//-------------------Works for "(()())(())" does not work for "(()())(())(()(()))"---------------------------
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count=0;
+        //count how many paranthese like=>(()())=>2
+        for(char i=1;i<s.length();i++){
+            char left='(';
+            char right=')';
+            if(s[i-1]==left && s[i]==right){
+                count++;
+            }
+        }
+        //Now generate and return count number of parantheses
+        string output="";
+        for(int i=0;i<count;i++){
+            output = output + "()";
+        }
+    return output;
+    }
+};
