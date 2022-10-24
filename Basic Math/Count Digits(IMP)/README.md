c6f89391d66bc97d94c0018138463f5fcea9e9a5
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-10-24 19:35:20 +0500

    Count Digits

---
+++ Basic Math/Count Digits(IMP)/README.md
@@ -0,0 +1,7 @@
+## Problem
+
+**https://practice.geeksforgeeks.org/problems/count-digits5716/1**
+
+## Solution
+
+- find reminder like rem of 42 is 2 and divide 42 by 10 => 4 untill temp != 0....
---
+++ Basic Math/Count Digits(IMP)/index.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+    int evenlyDivides(int N){
+        int num=0;
+        int temp=N;
+        while(temp!=0){
+            int rem=temp%10;
+            temp=temp/10;
+            if(rem>0&&N%rem==0){
+                num++;
+            }
+        }
+return num;
+    }
+};
+//Dry-Run
+//input 42
+//iteration 1 rem = 2 and temp becomes 4
+//iteration 2 rem = 4 and temp becomes 0 (4/10 is 0....something)
