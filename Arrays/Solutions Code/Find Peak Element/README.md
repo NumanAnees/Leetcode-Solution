d7b77aacb0bdb31d72e988e11d2e595b8c664aa8
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-11-09 20:52:32 +0500

    Peak element

---
+++ Arrays/Solutions Code/Find Peak Element/README.md
@@ -0,0 +1,4 @@
+## Problem
+- **https://leetcode.com/problems/find-peak-element/**
+## Solution
+- Apply binary search but remember mid not mid+1 or mid-1....
---
+++ Arrays/Solutions Code/Find Peak Element/index.cpp
@@ -0,0 +1,54 @@
+
+//-----------------------------------------------My better approach with 11 test cases------------------------
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        // base condition
+        if (nums.size() == 2)
+        {
+            if (nums[0] > nums[1])
+                return 0;
+        }
+        int start = 0;
+        int end = nums.size();
+        int mid = start + (end - start) / 2;
+        while (start < end - 2)
+        {
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            {
+                break;
+            }
+            else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1])
+            {
+                start = mid;
+                mid = start + (end - start) / 2;
+            }
+            else if (nums[mid] < nums[mid - 1] && nums[mid] > nums[mid + 1])
+            {
+                end = mid;
+                mid = start + (end - start) / 2;
+            }
+        }
+        return mid;
+    }
+};
+//---------------------------------------------------------All test cases------------------------------------
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int left = 0, right = nums.size() - 1;
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] < nums[mid + 1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+};
