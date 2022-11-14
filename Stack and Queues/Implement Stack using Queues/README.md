0158e6ced86880ab9a2d3f7c3189575f31b3a7ba
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-11-14 23:25:23 +0500

    stack using queues

---
+++ Stack and Queues/Implement Stack using Queues/README.md
@@ -0,0 +1,9 @@
+## Problem
+
+- **https://leetcode.com/problems/implement-stack-using-queues/**
+
+## Solution
+
+- Solve it using a single queue.
+- Push in a queue then reverse(push -> pop till queue length-1).
+- Top in stack and Front in Queues.
---
+++ Stack and Queues/Implement Stack using Queues/index.cpp
@@ -0,0 +1,90 @@
+//-----------------------------------------------------------------------------------------------------
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Stack {
+  queue < int > q;
+  public:
+    void Push(int x) {
+      int s = q.size();
+      q.push(x);
+      for (int i = 0; i < s; i++) {
+
+        q.push(q.front());
+        q.pop();
+      }
+    }
+  int Pop() {
+    int n = q.front();
+    q.pop();
+    return n;
+  }
+  int Top() {
+    return q.front();
+  }
+  int Size() {
+    return q.size();
+  }
+};
+
+int main() {
+  Stack s;
+  s.Push(3);
+  s.Push(2);
+  s.Push(4);
+  s.Push(1);
+  cout << "Top of the stack: " << s.Top() << endl;
+  cout << "Size of the stack before removing element: " << s.Size() << endl;
+  cout << "The deleted element is: " << s.Pop() << endl;
+  cout << "Top of the stack after removing element: " << s.Top() << endl;
+  cout << "Size of the stack after removing element: " << s.Size();
+
+}
+//---------------------------------------------Leetcode Version----------------------------------------
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    queue<int> q1;
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        int sz = q1.size();
+        q1.push(x);
+        while(sz--){
+            q1.push(q1.front());
+            q1.pop();
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int result = top();
+        q1.pop();
+        return result;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q1.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
