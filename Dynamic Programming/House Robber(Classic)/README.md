1f00789e4b339956b976d653d01bdbe2bde19769
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2023-02-25 13:01:26 +0500

    House Robber

---
+++ Dynamic Programming/House Robber(Classic)/README.md
@@ -0,0 +1,8 @@
+## Problem
+
+https://leetcode.com/problems/house-robber/
+
+## Solution
+
+- We find for each index which one is max. Pick and non pick case.
+- Dry run done.... for input `[1,2,3,1]`.
---
+++ Dynamic Programming/House Robber(Classic)/index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+  int Tabulation(vector<int> nums){
+        if(nums.size()==1)return nums[0];
+        int n=nums.size();
+        vector <int> dp(n,0);
+        //dp[0]=nums[0];
+        int max0=nums[0];
+        // dp[1]=max(nums[0],nums[1]);
+        int max1=max(nums[1],nums[0]);
+        for(int i=2;i<nums.size();i++){
+            // int inc=dp[i-2]+nums[i];
+            int inc=max0+nums[i];
+            // int exc=dp[i-1]+0;
+            int exc=max1+0;
+            max0 = max1;
+            max1 = max(inc,exc);
+        
+        }
+        return max1;
+    }
+public:
+    int rob(vector<int>& nums) {
+        return Tabulation(nums);
+    }
+};
