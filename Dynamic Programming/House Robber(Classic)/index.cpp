443fa8d94cd8b154e76644cfd1d4b52afd477411
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2023-03-28 00:57:05 +0500

    House Robber simple

--- Dynamic Programming/House Robber(Classic)/index.cpp
+++ Dynamic Programming/House Robber(Classic)/index.cpp
@@ -24,3 +24,25 @@ public:
         return Tabulation(nums);
     }
 };
+
+//
+class Solution {
+private:
+  int Tabulation(vector<int> nums){
+        if(nums.size()==1)return nums[0];
+        int n=nums.size();
+        vector <int> dp(n,0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            int inc=dp[i-2]+nums[i];
+            int exc=dp[i-1]+0;
+            dp[i] = max(inc,exc);
+        }
+        return dp[n-1];
+    }
+public:
+    int rob(vector<int>& nums) {
+        return Tabulation(nums);
+    }
+};
