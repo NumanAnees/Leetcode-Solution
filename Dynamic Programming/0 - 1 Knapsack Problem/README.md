de5493776c355d59f4d53cf632f1e317ebd0def6
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2023-03-28 01:33:56 +0500

    0/1 knapsack

---
+++ Dynamic Programming/0 - 1 Knapsack Problem/README.md
@@ -0,0 +1,3 @@
+## Problem
+
+https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=0-1-knapsack-problem0945
---
+++ Dynamic Programming/0 - 1 Knapsack Problem/index.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+private:
+    int knapsackUtil(int wt[], int val[], int ind, int W, vector<vector<int>>& dp){
+
+    if(ind == 0){
+        if(wt[0] <=W) return val[0];
+        else return 0;
+    }
+    
+    if(dp[ind][W]!=-1)
+        return dp[ind][W];
+        
+    int notTaken = 0 + knapsackUtil(wt,val,ind-1,W,dp);
+    
+    int taken = INT_MIN;
+    if(wt[ind] <= W)
+        taken = val[ind] + knapsackUtil(wt,val,ind-1,W-wt[ind],dp);
+        
+    return dp[ind][W] = max(notTaken,taken);
+    }
+public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        vector<vector<int>> dp(n,vector<int>(W+1,-1));
+        return knapsackUtil(wt, val, n-1, W, dp);
+    }
+};
