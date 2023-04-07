1cf550eef481909350c0eeb6b6b02795f25ec2d0
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2023-04-07 15:23:57 +0500

    Swap Nodes in Pairs

---
+++ Linked List/Swap Nodes in Pairs/README.md
@@ -0,0 +1,7 @@
+## Problem
+
+https://leetcode.com/problems/swap-nodes-in-pairs/
+
+## Solution
+
+- Just use the same approach of Reverse Nodes in K-Group.
---
+++ Linked List/Swap Nodes in Pairs/index.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* newHead = head->next;
+        ListNode* otherIter = newHead->next;
+        newHead->next=head;
+        head->next=swapPairs(otherIter);
+        return newHead;
+    }
+};
